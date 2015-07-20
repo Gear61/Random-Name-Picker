3fb2304deb2021833eb11bac3f03e1a0c457d691
Gear61 <chioualexander@yahoo.com>
2015-07-19 23:58:01 -0700

    v1 complete

--- app/src/main/java/com/randomappsinc/studentpicker/Activities/StudentChoosingActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Activities/StudentChoosingActivity.java
@@ -1,6 +1,8 @@
 package com.randomappsinc.studentpicker.Activities;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
@@ -10,6 +12,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.randomappsinc.studentpicker.Adapters.StudentChoosingAdapter;
 import com.randomappsinc.studentpicker.R;
@@ -21,6 +24,8 @@ public class StudentChoosingActivity extends ActionBarActivity
 {
     public static final String NO_STUDENTS = "You currently do not have any students in this list. " +
             "Why are you even here?";
+    public static final String NO_CHOICES = "There aren't any students to choose from.";
+    public static final String CHOSEN_STUDENT_DIALOG_TITLE = "And the (un)lucky winner is...";
 
     private Context context;
     private ListView students;
@@ -51,7 +56,31 @@ public class StudentChoosingActivity extends ActionBarActivity
 
     public void choose(View view)
     {
-
+        if (studentChoosingAdapter.getCount() == 0)
+        {
+            Toast.makeText(context, NO_CHOICES, Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            final String randomStudent = studentChoosingAdapter.chooseStudentAtRandom(withReplacement.isChecked());
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
+                    context);
+            alertDialogBuilder.setTitle(CHOSEN_STUDENT_DIALOG_TITLE);
+            alertDialogBuilder
+                    .setMessage(randomStudent)
+                            // Back button cancel dialog
+                    .setCancelable(true)
+                    .setPositiveButton("Ok", new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface dialog, int id)
+                        {
+                            dialog.dismiss();
+                        }
+                    });
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.setCanceledOnTouchOutside(true);
+            alertDialog.show();
+        }
     }
 
     @Override
