e9cf057334bbe4f063847ca8c73e62de8294965c
Gear61 <chioualexander@yahoo.com>
2015-10-28 23:56:21 -0700

    Add ripple effect to all listview items and tabs

--- app/src/main/java/com/randomappsinc/studentpicker/Layouts/SlidingTabStrip.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Layouts/SlidingTabStrip.java
@@ -12,12 +12,10 @@ import android.widget.LinearLayout;
 /**
  * Created by alexanderchiou on 10/18/15.
  */
-class SlidingTabStrip extends LinearLayout
-{
-
+class SlidingTabStrip extends LinearLayout {
     private static final int DEFAULT_BOTTOM_BORDER_THICKNESS_DIPS = 2;
     private static final byte DEFAULT_BOTTOM_BORDER_COLOR_ALPHA = 0x26;
-    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 8;
+    private static final int SELECTED_INDICATOR_THICKNESS_DIPS = 5;
     private static final int DEFAULT_SELECTED_INDICATOR_COLOR = 0xFF33B5E5;
 
     private static final int DEFAULT_DIVIDER_THICKNESS_DIPS = 1;
