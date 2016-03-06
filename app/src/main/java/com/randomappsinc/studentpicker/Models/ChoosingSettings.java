8664464af91b3df88e1325ad5d771854c4236c6e
Gear61 <chessnone@yahoo.com>
2016-03-06 13:32:56 -0800

    UI for presentation mode

--- app/src/main/java/com/randomappsinc/studentpicker/Models/ChoosingSettings.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Models/ChoosingSettings.java
@@ -4,12 +4,22 @@ package com.randomappsinc.studentpicker.Models;
  * Created by alexanderchiou on 2/15/16.
  */
 public class ChoosingSettings {
+    private boolean presentationMode;
     private boolean withReplacement;
     private int numNamesToChoose;
 
-    public ChoosingSettings(boolean withReplacement, int numNamesToChoose) {
+    public ChoosingSettings(boolean presentationMode, boolean withReplacement, int numNamesToChoose) {
+        this.presentationMode = presentationMode;
         this.withReplacement = withReplacement;
         this.numNamesToChoose = numNamesToChoose;
+    }
+
+    public boolean getPresentationMode() {
+        return presentationMode;
+    }
+
+    public void setPresentationMode(boolean presentationMode) {
+        this.presentationMode = presentationMode;
     }
 
     public boolean getWithReplacement() {
--- app/src/main/java/com/randomappsinc/studentpicker/Models/ChoosingSettingsViewHolder.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Models/ChoosingSettingsViewHolder.java
@@ -13,12 +13,14 @@ import butterknife.ButterKnife;
  * Created by alexanderchiou on 11/9/15.
  */
 public class ChoosingSettingsViewHolder {
+    @Bind(R.id.presentation_mode) public CheckBox presentationMode;
     @Bind(R.id.with_replacement) public CheckBox withReplacement;
     @Bind(R.id.num_people_chosen) public EditText numChosen;
 
     public ChoosingSettingsViewHolder(View view, ChoosingSettings settings) {
         ButterKnife.bind(this, view);
 
+        presentationMode.setCheckedImmediately(settings.getPresentationMode());
         withReplacement.setCheckedImmediately(settings.getWithReplacement());
         numChosen.setText(String.valueOf(settings.getNumNamesToChoose()));
     }
