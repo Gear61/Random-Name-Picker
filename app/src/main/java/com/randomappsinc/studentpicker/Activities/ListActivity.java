2e6c81a5248bde33f9b87a613655ce17fe239873
Gear61 <chessnone@yahoo.com>
2017-05-10 14:16:15 -0700

    Choose name on shake

--- app/src/main/java/com/randomappsinc/studentpicker/Activities/ListActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Activities/ListActivity.java
@@ -2,14 +2,18 @@ package com.randomappsinc.studentpicker.Activities;
 
 import android.app.Fragment;
 import android.content.res.Configuration;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 
+import com.afollestad.materialdialogs.MaterialDialog;
 import com.randomappsinc.studentpicker.Adapters.ListTabsAdapter;
 import com.randomappsinc.studentpicker.Fragments.EditNameListFragment;
 import com.randomappsinc.studentpicker.Fragments.NameChoosingFragment;
 import com.randomappsinc.studentpicker.R;
+import com.randomappsinc.studentpicker.Utils.PreferencesManager;
 import com.randomappsinc.studentpicker.Views.SlidingTabLayout;
+import com.squareup.seismic.ShakeDetector;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -17,11 +21,12 @@ import butterknife.ButterKnife;
 /**
  * Created by alexanderchiou on 10/18/15.
  */
-public class ListActivity extends StandardActivity {
+public class ListActivity extends StandardActivity implements ShakeDetector.Listener {
     @Bind(R.id.viewpager) ViewPager mViewPager;
     @Bind(R.id.sliding_tabs) SlidingTabLayout mSlidingTabLayout;
 
     private ListTabsAdapter listTabsAdapter;
+    private ShakeDetector shakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +41,17 @@ public class ListActivity extends StandardActivity {
         mViewPager.setAdapter(listTabsAdapter);
         mSlidingTabLayout.setCustomTabView(R.layout.custom_tab, R.id.tab_name);
         mSlidingTabLayout.setViewPager(mViewPager);
+
+        shakeDetector = new ShakeDetector(this);
+
+        if (PreferencesManager.get().shouldShowShake()) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.shake_it)
+                    .content(R.string.shake_now_supported)
+                    .positiveText(android.R.string.yes)
+                    .cancelable(false)
+                    .show();
+        }
     }
 
     @Override
@@ -49,6 +65,7 @@ public class ListActivity extends StandardActivity {
         if (editNameListFragment != null) {
             getFragmentManager().putFragment(savedInstanceState, EditNameListFragment.SCREEN_NAME, editNameListFragment);
         }
+        shakeDetector.stop();
         super.onSaveInstanceState(savedInstanceState);
     }
 
@@ -60,5 +77,22 @@ public class ListActivity extends StandardActivity {
 
     public ListTabsAdapter getListTabsAdapter() {
         return listTabsAdapter;
+    }
+
+    @Override
+    public void hearShake() {
+        listTabsAdapter.getNameChoosingFragment().choose();
+    }
+
+    @Override
+    public void onResume() {
+        shakeDetector.start((SensorManager) getSystemService(SENSOR_SERVICE));
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        shakeDetector.stop();
+        super.onDestroy();
     }
 }
