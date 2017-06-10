14b1d412f73a9a3e0b2f949f180d17849795a717
Gear61 <chioualexander@yahoo.com>
2017-06-09 23:06:41 -0700

    Upgrade DB to add count column

--- app/src/main/java/com/randomappsinc/studentpicker/Database/DataSource.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Database/DataSource.java
@@ -7,7 +7,9 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by alexanderchiou on 7/19/15.
@@ -28,7 +30,7 @@ public class DataSource {
     }
 
     // Terminate connection to database
-    public void close() {
+    private void close() {
         dbHelper.close();
     }
 
@@ -38,7 +40,7 @@ public class DataSource {
         String[] columns = {MySQLiteHelper.COLUMN_PERSON_NAME};
         String selection = MySQLiteHelper.COLUMN_LIST_NAME + " = ?";
         String[] selectionArgs = {listName};
-        Cursor cursor = database.query(MySQLiteHelper.PERSON_NAMES_TABLE_NAME, columns, selection,
+        Cursor cursor = database.query(MySQLiteHelper.TABLE_NAME, columns, selection,
                 selectionArgs, null, null, null);
         while (cursor.moveToNext()) {
             names.add(cursor.getString(0));
@@ -48,12 +50,13 @@ public class DataSource {
         return names;
     }
 
-    public void addName(String name, String listName) {
+    public void addName(String name, String listName, int amount) {
         open();
         ContentValues values = new ContentValues();
         values.put(MySQLiteHelper.COLUMN_LIST_NAME, listName);
         values.put(MySQLiteHelper.COLUMN_PERSON_NAME, name);
-        database.insert(MySQLiteHelper.PERSON_NAMES_TABLE_NAME, null, values);
+        values.put(MySQLiteHelper.COLUMN_NAME_COUNT, amount);
+        database.insert(MySQLiteHelper.TABLE_NAME, null, values);
         close();
     }
 
@@ -61,18 +64,18 @@ public class DataSource {
         open();
         long numInstances = getNumNamesInList(name, listName);
         String whereArgs[] = {name, listName};
-        database.delete(MySQLiteHelper.PERSON_NAMES_TABLE_NAME, MySQLiteHelper.COLUMN_PERSON_NAME + " = ? AND " +
+        database.delete(MySQLiteHelper.TABLE_NAME, MySQLiteHelper.COLUMN_PERSON_NAME + " = ? AND " +
                 MySQLiteHelper.COLUMN_LIST_NAME + " = ?", whereArgs);
         close();
         for (int i = 0; i < numInstances - 1; i++) {
-            addName(name, listName);
+            addName(name, listName, 1);
         }
     }
 
     public void deleteList(String listName) {
         open();
         String whereArgs[] = {listName};
-        database.delete(MySQLiteHelper.PERSON_NAMES_TABLE_NAME, MySQLiteHelper.COLUMN_LIST_NAME + " = ?", whereArgs);
+        database.delete(MySQLiteHelper.TABLE_NAME, MySQLiteHelper.COLUMN_LIST_NAME + " = ?", whereArgs);
         close();
     }
 
@@ -82,7 +85,7 @@ public class DataSource {
         newValues.put(MySQLiteHelper.COLUMN_LIST_NAME, newListName);
         String[] whereArgs = new String[]{oldListName};
         String whereStatement = MySQLiteHelper.COLUMN_LIST_NAME + "=?";
-        database.update(MySQLiteHelper.PERSON_NAMES_TABLE_NAME, newValues, whereStatement, whereArgs);
+        database.update(MySQLiteHelper.TABLE_NAME, newValues, whereStatement, whereArgs);
         close();
     }
 
@@ -90,7 +93,7 @@ public class DataSource {
         List<String> matchingNames = new ArrayList<>();
         open();
         Cursor cursor = database.rawQuery("SELECT DISTINCT " + MySQLiteHelper.COLUMN_PERSON_NAME +
-                " FROM " + MySQLiteHelper.PERSON_NAMES_TABLE_NAME + " WHERE " +
+                " FROM " + MySQLiteHelper.TABLE_NAME + " WHERE " +
                 MySQLiteHelper.COLUMN_PERSON_NAME + " like ? COLLATE NOCASE " +
                 "ORDER BY " + MySQLiteHelper.COLUMN_PERSON_NAME + " ASC", new String[] {prefix + "%"});
         while (cursor.moveToNext()) {
@@ -105,7 +108,7 @@ public class DataSource {
         List<String> names = new ArrayList<>();
         open();
         String[] columns = {MySQLiteHelper.COLUMN_LIST_NAME};
-        Cursor cursor = database.query(true, MySQLiteHelper.PERSON_NAMES_TABLE_NAME, columns, null, null, null, null, null, null);
+        Cursor cursor = database.query(true, MySQLiteHelper.TABLE_NAME, columns, null, null, null, null, null, null);
         while (cursor.moveToNext()) {
             if (!cursor.getString(0).equals(currentList)) {
                 names.add(cursor.getString(0));
@@ -121,7 +124,7 @@ public class DataSource {
         for (String listName : givingLists) {
             List<String> namesToImport = getAllNamesInList(listName);
             for (String name : namesToImport) {
-                addName(name, receivingList);
+                addName(name, receivingList, 1);
                 newNames.add(name);
             }
         }
@@ -129,13 +132,13 @@ public class DataSource {
     }
 
     public long getNumNamesInList(String name, String listName) {
-        return DatabaseUtils.queryNumEntries(database, MySQLiteHelper.PERSON_NAMES_TABLE_NAME,
+        return DatabaseUtils.queryNumEntries(database, MySQLiteHelper.TABLE_NAME,
                 MySQLiteHelper.COLUMN_LIST_NAME + " = ? AND " + MySQLiteHelper.COLUMN_PERSON_NAME + " = ?",
                 new String[] {listName, name});
     }
 
     public void renamePerson(String oldName, String newName, String listName) {
         removeName(oldName, listName);
-        addName(newName, listName);
+        addName(newName, listName, 1);
     }
 }
--- app/src/main/java/com/randomappsinc/studentpicker/Database/MySQLiteHelper.java
+++ app/src/main/java/com/randomappsinc/studentpicker/Database/MySQLiteHelper.java
@@ -1,28 +1,41 @@
 package com.randomappsinc.studentpicker.Database;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.randomappsinc.studentpicker.Utils.MyApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by alexanderchiou on 7/19/15.
  */
 public class MySQLiteHelper extends SQLiteOpenHelper {
     // Table name
-    public static final String PERSON_NAMES_TABLE_NAME = "Students";
+    public static final String TABLE_NAME = "Students";
 
     // COLUMNS
     public static final String COLUMN_LIST_NAME = "list_name";
     public static final String COLUMN_PERSON_NAME = "student_name";
+    public static final String COLUMN_NAME_COUNT = "name_count";
 
     // Some random things fed to a super's method
     private static final String DATABASE_NAME = "studentpicker.db";
-    private static final int DATABASE_VERSION = 1;
+    private static final int DATABASE_VERSION = 2;
 
     // Database creation sql statements
-    static final String STUDENTS_CREATE = "CREATE TABLE IF NOT EXISTS " + PERSON_NAMES_TABLE_NAME + "(" + COLUMN_LIST_NAME
-            + " TEXT, " + COLUMN_PERSON_NAME + " TEXT);";
+    private static final String STUDENTS_CREATE = "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "(" + COLUMN_LIST_NAME
+            + " TEXT, " + COLUMN_PERSON_NAME + " TEXT, " + COLUMN_NAME_COUNT + "INTEGER);";
+
+    // Updates
+    // V2
+    private static final String ADD_COUNT_COLUMN = "ALTER TABLE " + TABLE_NAME +
+            " ADD COLUMN " + COLUMN_NAME_COUNT + " INTEGER;";
 
     public MySQLiteHelper() {
         super(MyApplication.getAppContext(), DATABASE_NAME, null, DATABASE_VERSION);
@@ -34,5 +47,55 @@ public class MySQLiteHelper extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {}
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion) {
+        if (oldVersion == 1) {
+            List<String> nonEmptyLists = getNonEmptyLists(database);
+
+            // List name -> (Name -> Count)
+            Map<String, Map<String, Integer>> oldData = new HashMap<>();
+            for (String listName : nonEmptyLists) {
+                oldData.put(listName, getNameCountsLegacy(database, listName));
+            }
+
+            database.execSQL(ADD_COUNT_COLUMN);
+            database.execSQL("DELETE FROM " + TABLE_NAME);
+
+            for (String listName : oldData.keySet()) {
+                Map<String, Integer> listData = oldData.get(listName);
+                for (String name : listData.keySet()) {
+                    ContentValues values = new ContentValues();
+                    values.put(MySQLiteHelper.COLUMN_LIST_NAME, listName);
+                    values.put(MySQLiteHelper.COLUMN_PERSON_NAME, name);
+                    values.put(MySQLiteHelper.COLUMN_NAME_COUNT, listData.get(name));
+                    database.insert(MySQLiteHelper.TABLE_NAME, null, values);
+                }
+            }
+        }
+    }
+
+    // V1 -> V2 upgrade, get list names
+    private List<String> getNonEmptyLists(SQLiteDatabase database) {
+        List<String> lists = new ArrayList<>();
+        String[] columns = {MySQLiteHelper.COLUMN_LIST_NAME};
+        Cursor cursor = database.query(true, MySQLiteHelper.TABLE_NAME, columns, null, null, null, null, null, null);
+        while (cursor.moveToNext()) {
+            lists.add(cursor.getString(0));
+        }
+        cursor.close();
+        return lists;
+    }
+
+    // V1 -> V2 upgrade, save legacy data
+    private Map<String, Integer> getNameCountsLegacy(SQLiteDatabase database, String listName) {
+        Map<String, Integer> names = new HashMap<>();
+        Cursor cursor = database.rawQuery("SELECT " + MySQLiteHelper.COLUMN_PERSON_NAME +
+                ", COUNT() FROM " + MySQLiteHelper.TABLE_NAME + " WHERE " +
+                MySQLiteHelper.COLUMN_LIST_NAME + " = ? " +
+                "GROUP BY " + MySQLiteHelper.COLUMN_PERSON_NAME, new String[] {listName});
+        while (cursor.moveToNext()) {
+            names.put(cursor.getString(0), cursor.getInt(1));
+        }
+        cursor.close();
+        return names;
+    }
 }
