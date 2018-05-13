cf0fee6f80ec83d472ae0ea5e726389d46cc8a8e
Gear61 <chioualexander@yahoo.com>
2018-05-12 23:06:32 -0700

    Remove Rey material

--- app/src/main/java/com/randomappsinc/studentpicker/models/SetTextSizeViewHolder.java
+++ app/src/main/java/com/randomappsinc/studentpicker/models/SetTextSizeViewHolder.java
@@ -2,35 +2,42 @@ package com.randomappsinc.studentpicker.models;
 
 import android.util.TypedValue;
 import android.view.View;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.randomappsinc.studentpicker.R;
 import com.randomappsinc.studentpicker.utils.PreferencesManager;
-import com.rey.material.widget.Slider;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class SetTextSizeViewHolder {
 
-    @BindView(R.id.text_size_slider) public Slider textSizeSlider;
+    @BindView(R.id.text_size_slider) public SeekBar textSizeSlider;
     @BindView(R.id.sample_text) public TextView sampleText;
 
     public SetTextSizeViewHolder(View view) {
         ButterKnife.bind(this, view);
-        textSizeSlider.setOnPositionChangeListener(sizeSetListener);
-        textSizeSlider.setValue((float) PreferencesManager.get().getPresentationTextSize(), false);
+        textSizeSlider.setOnSeekBarChangeListener(sizeSetListener);
+        textSizeSlider.setProgress(PreferencesManager.get().getPresentationTextSize() - 1);
+        textSizeSlider.jumpDrawablesToCurrentState();
     }
 
-    Slider.OnPositionChangeListener sizeSetListener = new Slider.OnPositionChangeListener() {
+    SeekBar.OnSeekBarChangeListener sizeSetListener = new SeekBar.OnSeekBarChangeListener() {
         @Override
-        public void onPositionChanged(Slider view, boolean fromUser, float oldPos,
-                                      float newPos, int oldValue, int newValue) {
-            sampleText.setTextSize(TypedValue.COMPLEX_UNIT_SP, newValue * 8);
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            sampleText.setTextSize(TypedValue.COMPLEX_UNIT_SP, (progress + 1) * 8);
         }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
     };
 
     public void revertSetting() {
-        textSizeSlider.setValue((float) PreferencesManager.get().getPresentationTextSize(), false);
+        textSizeSlider.setProgress(PreferencesManager.get().getPresentationTextSize() - 1);
+        textSizeSlider.jumpDrawablesToCurrentState();
     }
 }
