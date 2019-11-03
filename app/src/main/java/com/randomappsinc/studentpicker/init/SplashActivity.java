1aef8c334fa5a5ab2981d15d054e789489fe654a
Gear61 <chessnone@yahoo.com>
2019-11-03 00:52:57 -0700

    Shorten copy

--- app/src/main/java/com/randomappsinc/studentpicker/init/SplashActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/init/SplashActivity.java
@@ -1,8 +1,10 @@
 package com.randomappsinc.studentpicker.init;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.randomappsinc.studentpicker.home.MainActivity;
 
 public class SplashActivity extends AppCompatActivity {
