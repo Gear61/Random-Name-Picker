b52408a95f67023255e5c46cfe8ed954465d1266
Gear61 <chioualexander@yahoo.com>
2019-11-03 15:19:22 -0800

    Remove unnecessary root activity check in MainActivity

--- app/src/main/java/com/randomappsinc/studentpicker/home/MainActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/home/MainActivity.java
@@ -60,15 +60,6 @@ public class MainActivity extends StandardActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Kill activity if it's above an existing stack due to launcher bug
-        if (!isTaskRoot()
-                && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER)
-                && getIntent().getAction() != null && getIntent().getAction().equals(Intent.ACTION_MAIN)) {
-            finish();
-            return;
-        }
-
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
