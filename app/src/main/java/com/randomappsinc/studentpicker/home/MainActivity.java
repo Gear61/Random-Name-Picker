8fb0ee9e05e735d9bd5bbd4c1c87b8d1f52a6573
Moham <m7md972013@gmail.com>
2019-11-30 15:06:55 +0200

    add padding to editText, and changed the string key name

--- app/src/main/java/com/randomappsinc/studentpicker/home/MainActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/home/MainActivity.java
@@ -246,7 +246,7 @@ public class MainActivity extends StandardActivity implements NameListsAdapter.D
         Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
         intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.speech_message));
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, getString(R.string.list_name_input_speech_message));
         try {
             startActivityForResult(intent, SPEECH_REQUEST_CODE);
         } catch (ActivityNotFoundException exception) {
