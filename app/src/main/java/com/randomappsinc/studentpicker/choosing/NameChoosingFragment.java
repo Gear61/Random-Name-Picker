36f99147de96f3a1f9309734cfef6bcc7254750e
Alaa Alzibda <alaa07996@gmail.com>
2020-01-10 11:38:17 +0200

    Formatting code

--- app/src/main/java/com/randomappsinc/studentpicker/choosing/NameChoosingFragment.java
+++ app/src/main/java/com/randomappsinc/studentpicker/choosing/NameChoosingFragment.java
@@ -2,14 +2,12 @@ package com.randomappsinc.studentpicker.choosing;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
