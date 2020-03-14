c8630e5d2a66cfdf34373041638a41890db41095
Gear61 <chessnone@yahoo.com>
2020-03-14 13:13:04 -0700

    Upgrade Gradle, turn shake off by default, remove confusing shake dialog, improve add name empty state

--- app/src/main/java/com/randomappsinc/studentpicker/listpage/ListActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/listpage/ListActivity.java
@@ -7,7 +7,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
 
-import com.afollestad.materialdialogs.MaterialDialog;
 import com.google.android.material.tabs.TabLayout;
 import com.randomappsinc.studentpicker.R;
 import com.randomappsinc.studentpicker.common.StandardActivity;
@@ -61,15 +60,6 @@ public class ListActivity extends StandardActivity implements ShakeDetector.List
         nameListTabs.setupWithViewPager(nameListPager);
 
         shakeDetector = new ShakeDetector(this);
-
-        if (preferencesManager.shouldShowShake()) {
-            new MaterialDialog.Builder(this)
-                    .title(R.string.shake_it)
-                    .content(R.string.shake_now_supported)
-                    .positiveText(android.R.string.yes)
-                    .cancelable(false)
-                    .show();
-        }
     }
 
     @OnPageChange(R.id.name_list_pager)
