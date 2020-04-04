c7fd33bcb769e3d7d499696cf837f03c17684cda
Gear61 <chioualexander@yahoo.com>
2020-04-04 13:05:45 -0700

    Search with speech

--- app/src/main/java/com/randomappsinc/studentpicker/utils/PermissionUtils.java
+++ app/src/main/java/com/randomappsinc/studentpicker/utils/PermissionUtils.java
@@ -6,6 +6,7 @@ import android.content.pm.PackageManager;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
 
 public class PermissionUtils {
 
@@ -16,5 +17,9 @@ public class PermissionUtils {
 
     public static void requestPermission(Activity activity, String permission, int requestCode) {
         ActivityCompat.requestPermissions(activity, new String[] {permission}, requestCode);
+    }
+
+    public static void requestPermission(Fragment fragment, String permission, int requestCode) {
+        fragment.requestPermissions(new String[]{permission}, requestCode);
     }
 }
