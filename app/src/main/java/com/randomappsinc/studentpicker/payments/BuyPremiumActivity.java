56ff0edf3727fdffb6507b335c1e61a5f2cd4796
Gear61 <chioualexander@yahoo.com>
2020-04-09 22:28:38 -0700

    Buy premium UI

--- app/src/main/java/com/randomappsinc/studentpicker/payments/BuyPremiumActivity.java
+++ app/src/main/java/com/randomappsinc/studentpicker/payments/BuyPremiumActivity.java
@@ -1,15 +1,20 @@
 package com.randomappsinc.studentpicker.payments;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.joanzapata.iconify.IconDrawable;
 import com.joanzapata.iconify.fonts.IoniconsIcons;
 import com.randomappsinc.studentpicker.R;
-import com.randomappsinc.studentpicker.common.StandardActivity;
 
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class BuyPremiumActivity extends StandardActivity {
+public class BuyPremiumActivity extends AppCompatActivity {
+
+    private PaymentManager paymentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,11 +28,28 @@ public class BuyPremiumActivity extends StandardActivity {
                 .setHomeAsUpIndicator(new IconDrawable(this, IoniconsIcons.ion_android_close)
                         .colorRes(R.color.white)
                         .actionBarSize());
+
+        paymentManager = new PaymentManager(this);
+    }
+
+    @OnClick(R.id.buy)
+    public void buy() {
+        paymentManager.startPaymentFlow();
     }
 
     @Override
     public void finish() {
         super.finish();
         overridePendingTransition(0, R.anim.slide_out_from_top);
+        paymentManager.cleanUp();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
