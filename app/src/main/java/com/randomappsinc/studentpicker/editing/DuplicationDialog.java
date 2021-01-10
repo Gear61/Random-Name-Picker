56c4eca4fbdf23a4e9db636824973d6e554ed4c5
Gear61 <chessnone@yahoo.com>
2021-01-09 17:24:11 -0800

    Add keyboard hack to all dialogs

--- app/src/main/java/com/randomappsinc/studentpicker/editing/DuplicationDialog.java
+++ app/src/main/java/com/randomappsinc/studentpicker/editing/DuplicationDialog.java
@@ -1,8 +1,11 @@
 package com.randomappsinc.studentpicker.editing;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.view.inputmethod.InputMethodManager;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -11,12 +14,15 @@ import com.randomappsinc.studentpicker.models.NameDO;
 
 public class DuplicationDialog {
 
+    private static final long MILLIS_DELAY_FOR_KEYBOARD = 250L;
+
     public interface Listener {
         void onDuplicationSubmitted(int nameId, String name, int amountToAdd);
     }
 
-    private MaterialDialog dialog;
+    private final MaterialDialog dialog;
     private NameDO currentName;
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     DuplicationDialog(Context context, final Listener listener) {
         dialog = new MaterialDialog.Builder(context)
@@ -38,9 +44,19 @@ public class DuplicationDialog {
         dialog.getInputEditText().setFilters(new InputFilter[] {new InputFilter.LengthFilter(3)});
     }
 
+    private void maybeRunShowKeyboardHack() {
+        handler.postDelayed(() -> {
+            if (dialog.getInputEditText().requestFocus()) {
+                InputMethodManager imm = (InputMethodManager) dialog.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(dialog.getInputEditText(), InputMethodManager.SHOW_IMPLICIT);
+            }
+        }, MILLIS_DELAY_FOR_KEYBOARD);
+    }
+
     public void show(NameDO name) {
         currentName = name;
         dialog.getInputEditText().setText("");
         dialog.show();
+        maybeRunShowKeyboardHack();
     }
 }
--- app/src/main/java/com/randomappsinc/studentpicker/home/RenameListDialog.java
+++ app/src/main/java/com/randomappsinc/studentpicker/home/RenameListDialog.java
@@ -1,6 +1,9 @@
 package com.randomappsinc.studentpicker.home;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.inputmethod.InputMethodManager;
 
 import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -8,11 +11,14 @@ import com.randomappsinc.studentpicker.R;
 
 public class RenameListDialog {
 
+    private static final long MILLIS_DELAY_FOR_KEYBOARD = 250L;
+
     public interface Listener {
         void onRenameListConfirmed(String newName);
     }
 
-    private MaterialDialog dialog;
+    private final MaterialDialog dialog;
+    private final Handler handler = new Handler(Looper.getMainLooper());
 
     public RenameListDialog(Listener listener, Context context) {
         dialog = new MaterialDialog.Builder(context)
@@ -33,8 +39,18 @@ public class RenameListDialog {
                 .build();
     }
 
+    private void maybeRunShowKeyboardHack() {
+        handler.postDelayed(() -> {
+            if (dialog.getInputEditText().requestFocus()) {
+                InputMethodManager imm = (InputMethodManager) dialog.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(dialog.getInputEditText(), InputMethodManager.SHOW_IMPLICIT);
+            }
+        }, MILLIS_DELAY_FOR_KEYBOARD);
+    }
+
     public void show(String currentName) {
         dialog.getInputEditText().setText(currentName);
         dialog.show();
+        maybeRunShowKeyboardHack();
     }
 }
