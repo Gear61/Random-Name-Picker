9c8a00978d2b2dac140c264e5eca5bfc403af7b4
mohammedhemaid <mohammed.hemaid@outlook.com>
2024-05-02 19:10:33 -0400

    Update targetSDK to 34 and some libraries to make it compatible with the update

--- app/src/main/java/com/randomappsinc/studentpicker/utils/PermissionUtils.java
+++ app/src/main/java/com/randomappsinc/studentpicker/utils/PermissionUtils.java
@@ -1,8 +1,11 @@
 package com.randomappsinc.studentpicker.utils;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Environment;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -11,6 +14,18 @@ import androidx.fragment.app.Fragment;
 public class PermissionUtils {
 
     public static boolean isPermissionGranted(String permission, Context context) {
+        if (permission.equals(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.Q) {
+                return true;
+            } else if (Build.VERSION.SDK_INT == Build.VERSION_CODES.Q && !Environment.isExternalStorageLegacy()) {
+                return true;
+            }
+        } else if (permission.equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                return true;
+            }
+        }
+
         return ContextCompat.checkSelfPermission(context, permission)
                 == PackageManager.PERMISSION_GRANTED;
     }
